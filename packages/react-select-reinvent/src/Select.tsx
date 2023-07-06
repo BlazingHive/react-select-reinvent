ac7c6141b99ac2979ef9f491d41787d257f52938
Alan <wangyueleng@gmail.com>
2023-07-06 17:24:36 -0400

    solve type issue

--- packages/react-select-reinvent/src/Select.tsx
+++ packages/react-select-reinvent/src/Select.tsx
@@ -394,7 +394,11 @@ function buildCategorizedOptions<
 ): CategorizedGroupOrOption<Option, Group>[] {
   return props.options
     .map((groupOrOption, groupOrOptionIndex) => {
-      if ("options" in groupOrOption) {
+      if (
+        typeof groupOrOption === "object" &&
+        groupOrOption &&
+        "options" in groupOrOption
+      ) {
         const categorizedOptions = groupOrOption.options
           .map((option, optionIndex) =>
             toCategorizedOption(props, option, selectValue, optionIndex)
